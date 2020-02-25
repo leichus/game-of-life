140a9fe93b831070ab640f1c7b9a58115e66ba03
Daniel Leichus <dpleichus@gmail.com>
2020-02-25 09:40:58 -0500

    Add button to advance one frame

--- client/src/components/Content.tsx
+++ client/src/components/Content.tsx
@@ -19,44 +19,61 @@ export const Content = () => {
   };
 
   const increaseHeight = () => {
-    setCurrentConfig(oldConfig => {
-      if (oldConfig.length >= 30) {
-        return oldConfig;
-      } else {
-        const numCols = oldConfig[0].length;
-        return [...oldConfig, Array(numCols).fill(false)];
-      }
-    });
+    if (currentConfig.length < 30) {
+      setCurrentConfig(config => [...config, Array(config[0].length).fill(false)]);
+    }
   };
 
   const increaseWidth = () => {
-    setCurrentConfig(oldConfig => {
-      if (oldConfig[0].length >= 30) {
-        return oldConfig;
-      } else {
-        return oldConfig.map(row => [...row, false]);
-      }
-    })
+    if (currentConfig[0].length < 30) {
+      setCurrentConfig(config => config.map(row => [...row, false]));
+    }
   };
 
   const decreaseHeight = () => {
-    setCurrentConfig(oldConfig => {
-      if (oldConfig.length <= 1) {
-        return oldConfig;
-      } else {
-        return oldConfig.slice(0, oldConfig.length - 1);
-      }
-    });
+    if (currentConfig.length > 1) {
+      setCurrentConfig(config => config.slice(0, config.length - 1));
+    }
   };
 
   const decreaseWidth = () => {
+    if (currentConfig[0].length > 1) {
+      setCurrentConfig(config => config.map(row => row.slice(0, row.length - 1)));
+    }
+  };
+
+  const advanceOneFrame = () => {
     setCurrentConfig(oldConfig => {
-      if (oldConfig[0].length <= 1) {
-        return oldConfig;
-      } else {
-        return oldConfig.map(row => row.slice(0, row.length-1))
+      const newConfig = [...oldConfig.map(row => [...row])];
+
+      const numRows = newConfig.length;
+      const numCols = newConfig[0].length;
+      for (let row = 0; row < numRows; row++) {
+        for (let col = 0; col < numCols; col++) {
+
+          const topLeft = (row > 0 && col > 0) ? oldConfig[row - 1][col - 1] : false;
+          const top = (row > 0) ? oldConfig[row - 1][col] : false;
+          const topRight = (row > 0 && col + 1 < numCols) ? oldConfig[row - 1][col + 1] : false;
+          const left = (col > 0) ? oldConfig[row][col - 1] : false;
+          const self = oldConfig[row][col];
+          const right = (col + 1 < numCols) ? oldConfig[row][col + 1] : false;
+          const bottomLeft = (row + 1 < numRows && col > 0) ? oldConfig[row + 1][col - 1] : false;
+          const bottom = (row + 1 < numRows) ? oldConfig[row + 1][col] : false;
+          const bottomRight = (row + 1 < numRows && col + 1 < numCols) ? oldConfig[row + 1][col + 1] : false;
+
+          const sumNeighbors = [topLeft, top, topRight, left, right, bottomLeft, bottom, bottomRight]
+            .reduce((accum, bool) => accum + +bool, 0);
+
+          if (!self && sumNeighbors === 3) {
+            newConfig[row][col] = true;
+          } else if (self && sumNeighbors !== 2 && sumNeighbors !== 3) {
+            newConfig[row][col] = false;
+          }
+        }
       }
-    });
+
+      return newConfig;
+    })
   };
 
   return (
@@ -68,6 +85,7 @@ export const Content = () => {
                increaseWidth={ increaseWidth }
                decreaseHeight={ decreaseHeight }
                increaseHeight={ increaseHeight }
+               advanceOneFrame={ advanceOneFrame }
       />
     </div>
   );
--- client/src/components/Toolbar.tsx
+++ client/src/components/Toolbar.tsx
@@ -5,20 +5,24 @@ interface ToolbarProps {
   increaseWidth: () => void;
   decreaseHeight: () => void;
   increaseHeight: () => void;
+  advanceOneFrame: () => void;
 }
 
 export const Toolbar = (props: ToolbarProps) => {
   return (
     <div className="toolbar">
-      <div className="toolbar-row">
+      <div className="toolbar-row-section">
         <button onClick={ props.decreaseWidth }>-</button>
         <div>Change Width</div>
         <button onClick={ props.increaseWidth }>+</button>
       </div>
-      <div className="toolbar-row">
+      <div className="toolbar-row-section">
         <button onClick={ props.decreaseHeight }>-</button>
         <div>Change Height</div>
         <button onClick={ props.increaseHeight }>+</button>
+      </div>
+      <div className="toolbar-row-section">
+        <button onClick={ props.advanceOneFrame }>▶️</button>
       </div>
     </div>
   )
