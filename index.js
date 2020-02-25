3834e8d8ffbd7ca074dddb1ad83d82aac8159c25
Daniel Leichus <dpleichus@gmail.com>
2020-02-25 17:04:07 -0500

    Simplify express backend; configure for CF deployment

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+module.exports = app;
